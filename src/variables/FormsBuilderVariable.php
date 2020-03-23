36d8b36678608f150b60e54eb3748cb71c5b5465
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-03-23 16:19:27 +0000

    Added ability to get form from predefined config

--- src/variables/FormsBuilderVariable.php
+++ src/variables/FormsBuilderVariable.php
@@ -3,6 +3,8 @@
 namespace craftplugins\formbuilder\variables;
 
 use craftplugins\formbuilder\models\Form;
+use craftplugins\formbuilder\Plugin;
+use yii\base\Exception;
 
 /**
  * Class FormsBuilderVariable
@@ -19,5 +21,22 @@ class FormsBuilderVariable
     public function getForm(array $config): Form
     {
         return new Form($config);
+    }
+
+    /**
+     * @param string $handle
+     *
+     * @return \craftplugins\formbuilder\models\Form
+     * @throws \yii\base\Exception
+     */
+    public function getFormByHandle(string $handle): Form
+    {
+        $forms = Plugin::getInstance()->getConfig()->forms;
+
+        if (empty($forms[$handle])) {
+            throw new Exception("No form with handle: {$handle}");
+        }
+
+        return $forms[$handle];
     }
 }

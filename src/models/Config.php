292633d8349230864a0c118292a9327bc53f60fb
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-03-23 17:33:34 +0000

    Expect Form instances to be provided in config

--- src/models/Config.php
+++ src/models/Config.php
@@ -5,10 +5,15 @@ namespace craftplugins\formbuilder\models;
 
 use craft\base\Model;
 
+/**
+ * Class Config
+ *
+ * @package craftplugins\formbuilder\models
+ */
 class Config extends Model
 {
     /**
-     * @var array
+     * @var \craftplugins\formbuilder\models\Form[]|array
      */
     public $forms = [];
 }

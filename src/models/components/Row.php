fe27b015f691d3b3ffd0720664c225470179678e
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-03-24 20:06:48 +0000

    Naming tweaks

--- src/models/components/Row.php
+++ src/models/components/Row.php
@@ -5,21 +5,27 @@ namespace craftplugins\formbuilder\models\components;
 use craftplugins\formbuilder\models\components\traits\HasComponentsTrait;
 use Twig\Markup;
 
+/**
+ * Class Row
+ *
+ * @package craftplugins\formbuilder\models\components
+ */
 class Row extends AbstractComponent
 {
     use HasComponentsTrait;
 
     /**
-     * @param array $fields
+     * @param array $components
      * @param array $config
      *
      * @return \craftplugins\formbuilder\models\components\AbstractComponent
      */
-    public static function create($fields = [], $config = []): AbstractComponent
+    public static function create($components = [], $config = []): AbstractComponent
     {
-        $config['fields'] = $fields;
+        $instance = new self($config);
+        $instance->setComponents($components);
 
-        return parent::create($config);
+        return $instance;
     }
 
     /**

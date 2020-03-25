8c7083262618451d50e5aa29230f233c0034cbb3
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-03-25 19:23:49 +0000

    Added submit button

---
+++ src/models/components/SubmitButtonField.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace craftplugins\formbuilder\models\components;
+
+
+use craftplugins\formbuilder\helpers\Html;
+
+/**
+ * Class SubmitButtonField
+ *
+ * @package craftplugins\formbuilder\models\components
+ */
+class SubmitButtonField extends ButtonField
+{
+    /**
+     * @var string
+     */
+    protected $buttonText = 'Submit';
+
+    /**
+     * @return string
+     */
+    public function getControlHtml(): string
+    {
+        return Html::submitButton(
+            $this->getButtonText(),
+            $this->getInputAttributes()
+        );
+    }
+}

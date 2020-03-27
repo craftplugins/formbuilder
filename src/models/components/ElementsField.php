052557f61e996b6eb8e5bb144f80b7ad95d54017
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-03-27 16:43:15 +0000

    Added elements field

---
+++ src/models/components/ElementsField.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace craftplugins\formbuilder\models\components;
+
+use craft\base\Element;
+use craftplugins\formbuilder\helpers\ArrayHelper;
+
+/**
+ * Class ElementsField
+ *
+ * @package craftplugins\formbuilder\models\components
+ * @property callable|null $elementsRenderer
+ */
+class ElementsField extends BaseField
+{
+    /**
+     * @var \craft\elements\db\ElementQuery|null
+     */
+    protected $elementQuery;
+
+    /**
+     * @var callable|null
+     */
+    protected $elementRenderer;
+
+    /**
+     * @var Element[]|null
+     */
+    protected $elements;
+
+    /**
+     * @return \craft\elements\db\ElementQuery|null
+     */
+    public function getElementQuery(): ?\craft\elements\db\ElementQuery
+    {
+        return $this->elementQuery;
+    }
+
+    /**
+     * @param \craft\elements\db\ElementQuery|null $elementQuery
+     *
+     * @return $this
+     */
+    public function setElementQuery(?\craft\elements\db\ElementQuery $elementQuery): self
+    {
+        $this->elementQuery = $elementQuery;
+
+        return $this;
+    }
+
+    /**
+     * @return \craft\base\Element[]|null
+     */
+    public function getElements(): ?array
+    {
+        $query = $this->getElementQuery();
+
+        if ($this->elements === null && $query !== null) {
+            $this->elements = $query->all();
+        }
+
+        return $this->elements;
+    }
+
+    /**
+     * @param array|null $elements
+     *
+     * @return $this
+     */
+    public function setElements(?array $elements): self
+    {
+        $this->elements = $elements;
+
+        return $this;
+    }
+
+    /**
+     * @return callable|null
+     */
+    public function getElementsRenderer(): ?callable
+    {
+        return $this->elementRenderer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFieldControlHtml(): string
+    {
+        $elements = $this->getElements();
+        $renderer = $this->getElementsRenderer();
+
+        if ($elements === null || $renderer === null) {
+            return '';
+        }
+
+        return $renderer($elements, $this);
+    }
+
+    /**
+     * @return $this
+     */
+    public function renderAsCheckboxGroupField(): self
+    {
+        return $this->setElementsRenderer(static function (array $elements, self $elementsField) {
+            return CheckboxGroupField::create()
+                ->setInputName($elementsField->getInputName())
+                ->setInputItems(ArrayHelper::map($elements, 'id', 'title'));
+        });
+    }
+
+    /**
+     * @return $this
+     */
+    public function renderAsRadioGroupField(): self
+    {
+        return $this->setElementsRenderer(static function (array $elements, self $elementsField) {
+            return RadioGroupField::create()
+                ->setInputName($elementsField->getInputName())
+                ->setInputItems(ArrayHelper::map($elements, 'id', 'title'));
+        });
+    }
+
+    /**
+     * @return $this
+     */
+    public function renderAsSelectField(): self
+    {
+        return $this->setElementsRenderer(static function (array $elements, self $elementsField) {
+            return SelectField::create()
+                ->setInputName($elementsField->getInputName())
+                ->setInputItems(ArrayHelper::map($elements, 'id', 'title'));
+        });
+    }
+
+    /**
+     * @param callable|null $elementRenderer
+     *
+     * @return $this
+     */
+    public function setElementsRenderer(?callable $elementRenderer): self
+    {
+        $this->elementRenderer = $elementRenderer;
+
+        return $this;
+    }
+}

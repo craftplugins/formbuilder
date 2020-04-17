c8891ea13a28b14c8ad59a997ae485d12f6723c0
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-04-17 15:00:38 +0100

    Added FileInputField

---
+++ src/models/components/FileInputField.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace craftplugins\formbuilder\models\components;
+
+/**
+ * Class FileInputField
+ *
+ * @package craftplugins\formbuilder\models\components
+ */
+class FileInputField extends InputField
+{
+    /**
+     * @var string
+     */
+    protected $inputType = 'file';
+}
